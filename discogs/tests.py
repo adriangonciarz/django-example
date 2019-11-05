26912e1a4b9e66c387bc74cac9671fef39ec5996
Adrian Gonciarz <adrian.gonciarz@synerise.com>
2019-11-05 20:46:10 +0100

    update files and add example test

--- discogs/tests.py
+++ discogs/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Artist
+from .views import artist_list
+
+class TestArtistListView(TestCase):
+    def setUp(self):
+        Artist.objects.create(name='Deep Purple', country='uk')
+        Artist.objects.create(name='Slayer', country='us')
+
+    def test_artist_list_view(self):
+        response = self.client.get(reverse('discogs:artist-list'))
+        self.assertContains(response, 'Deep Purple')
+        self.assertTemplateUsed(response, 'discogs/artist_list.html')
